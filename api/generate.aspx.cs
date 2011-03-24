0def1eb886fc0a17c82f4f1875dfa9d20b5e66bc
Ryan Shaul <terandle@gmail.com>
2011-03-23 17:20:27 -0700

    Stuff

--- api/generate.aspx.cs
+++ api/generate.aspx.cs
@@ -4,14 +4,14 @@ using Dig;
 public partial class api_generate : ApiPage
 {
 	public override string GetResponse() {
-		string email = Request.QueryString["email"];
+		string login = Request.QueryString["login"];
 		string password = Dig.User.Hash(Request.QueryString["password"]);
 
 		User user;
-		if (UserStore.TryGetUser(email, password, out user)) {
+		if (UserStore.TryGetUser(login, password, out user)) {
 			return KeyStore.Generate(user).FormatCode();
 		} else {
-			return "Error: Invalid login";
+			return "Error: Invalid login or password";
 		}
 	}
 }
